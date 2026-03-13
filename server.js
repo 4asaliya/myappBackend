8fecf7d0bc9747772d397fc397174009f4275e41
Your Name <you@example.com>
2026-03-13 21:30:06 +0600

    post delete put get works

--- server.js
+++ server.js
@@ -26,6 +26,58 @@ app.get("/todos", async (req, res) => {
   }
 });
 
+// CREATE todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { todo_text } = req.body;
+
+    const result = await pool.query(
+      "INSERT INTO todo (todo_text, is_completed) VALUES ($1, false) RETURNING *",
+      [todo_text]
+    );
+
+    res.json(result.rows[0]);
+
+  } catch (error) {
+    console.error("Error creating todo:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// UPDATE todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+
+    const { id } = req.params;
+    const { todo_text, is_completed } = req.body;
+
+    const result = await pool.query(
+      "UPDATE todo SET todo_text = $1, is_completed = $2 WHERE id = $3 RETURNING *",
+      [todo_text, is_completed, id]
+    );
+
+    res.json(result.rows[0]);
+
+  } catch (error) {
+    console.error("Error updating todo:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// DELETE todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await pool.query("DELETE FROM todo WHERE id = $1", [id]);
+
+    res.json({ message: "Todo deleted" });
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 //Get Health
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -34,5 +86,7 @@ app.get("/", (req, res) => {
 //To run server at port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
+  console.log(`Server running on port ${PORT}`);
 });
+
+//curl -X POST http://localhost:5000/todos -H "Content-Type:application/json" -d'("title":"Buy groceries")'
